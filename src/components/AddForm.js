e17d437e0efe1e49a8e89d88e50c19174fa8678f
KRQuinn <kr.quinn82@gmail.com>
2021-09-10 16:13:43 -0700

    MVP complete

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux'
+import { errorSmurf, addSmurf } from '../actions'
 
 const AddForm = (props) => {
     const [state, setState] = useState({
@@ -18,7 +20,10 @@ const AddForm = (props) => {
     const handleSubmit = e => {
         e.preventDefault();
         if (state.name === "" || state.position === "" || state.nickname === "") {
-            errorMessage = "Name, position and nickname fields are required.";
+            props.errorSmurf = "Name, position and nickname fields are required.";
+        }
+        else{
+            props.addSmurf(state)
         }
     }
 
@@ -51,7 +56,13 @@ const AddForm = (props) => {
     </section>);
 }
 
-export default AddForm;
+const mapStateToProps = state => {
+    return({
+        error: state.error
+    })
+}
+
+export default connect(mapStateToProps, { addSmurf, errorSmurf})(AddForm);
 
 //Task List:
 //1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
+import { applyMiddleware, createStore } from "redux";
 
 import "./index.css";
 import App from "./App";
@@ -9,8 +14,12 @@ worker.start();
 
 const rootElement = document.getElementById("root");
 
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     rootElement
 );
 
